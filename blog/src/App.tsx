6ae6d63c3e54eeeab204237adf8b43b0d3bee5c2
xueminghao <minghaoxue@clubfactory.com>
2021-04-16 12:22:23 +0800

    feat: fetch post from github issue

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
+import { Octokit } from '@octokit/rest'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  html_url: string;
+}
+
+export interface IAppState {
+  posts: Post[];
+}
+
+class App extends PureComponent<{}, IAppState> {
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      posts: [],
+    }
+  }
+
+  componentDidMount(): void {
+    this._loadPosts()
+  }
+
+  render(): JSX.Element {
+    return <ol>
+      {this.state.posts.map(post => {
+        return <li key={post.id}>
+          <a href={post.html_url}>
+          {post.title}
+          </a>
+        </li>
+      })}
+    </ol>
+  }
+
+  _loadPosts(): void {
+    const octokit = new Octokit({
+      auth: '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',
+      userAgent: 'blog client',
+      baseUrl: 'https://api.github.com',
+  
+    })
+    const owner = 'xueminghao'
+    const repo = 'xueminghao.github.io'
+    octokit.rest.issues.listForRepo({
+      owner,
+      repo,
+    }).then(res => {
+      if (res?.data) {
+        const posts: Post[] = res.data.map((item) => {
+          return {
+            id: item.id,
+            title: item.title,
+            body: item.body,
+            html_url: item.html_url
+          } as Post
+        })
+        this.setState({
+          posts: posts,
+        })
+      }
+      console.log(res)
+    })
+  }
 }
 
 export default App;
